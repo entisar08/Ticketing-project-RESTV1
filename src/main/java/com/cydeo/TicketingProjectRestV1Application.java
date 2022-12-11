a78f79ce32f076848bb8d8494f3e064ca6a3ba1d
entisar08 <eelmohbat@gmail.com>
2022-12-10 23:52:34 -0600

    Project created dependency added

---
+++ src/main/java/com/cydeo/TicketingProjectRestV1Application.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectRestV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectRestV1Application.class, args);
+    }
+
+}
