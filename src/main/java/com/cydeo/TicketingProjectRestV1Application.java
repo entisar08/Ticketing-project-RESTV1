6bb207e8effc6d1f6702a6538fdd863ccf5a820e
entisar08 <eelmohbat@gmail.com>
2022-12-11 10:27:39 -0600

    Updating Pom file and creating Model Mapper

--- src/main/java/com/cydeo/TicketingProjectRestV1Application.java
+++ src/main/java/com/cydeo/TicketingProjectRestV1Application.java
@@ -2,6 +2,7 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectRestV1Application {
@@ -9,5 +10,16 @@ public class TicketingProjectRestV1Application {
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectRestV1Application.class, args);
     }
+
+
+
+
+
+    @Bean
+    public ModelMapper mapper(){
+
+        return new ModelMapper();
+    }
+
 
 }
